3acf3ac2602abd3b1281af271fdfc2fd8153e736
Devyanshi Sitani <devyanshi1102@gmail.com>
2024-06-08 11:49:58 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Devyanshi Sitani",
+  title: "Web Developer",
+  email: "devyanshi1102@gmail.com",
+  gitHub: "DevyanshiS",
+  instagram: "",
+  linkedIn: "Devyanshi Sitani",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desk.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My frist basic react site",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "It adds students after taking inout of their roll number and name.",
+    url: "https://devyanshi-first-reactapp.netlify.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Calendar",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is a calendar",
+    url: "https://devyanshis.github.io/calendar/",
   },
   {
-    title: "My Resume Site",
+    title: "My LinkedIn",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This includes my positions held and experience.",
+    url: "https://www.linkedin.com/in/devyanshi-sitani-006326205?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Calculator",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is a calculator",
+    url: " https://devyanshis.github.io/calulator/",
   },
 ];
 
